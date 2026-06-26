449df957ede2c5f45dc5e4c7e525e9e926012404
Vlad <vladyuzevuts@gmail.com>
2026-06-26 11:20:04 +0300

    Create JWT token service

---
+++ backend/src/MoodboardAI.Api/Services/IJwtTokenService.cs
@@ -0,0 +1,12 @@
+namespace MoodboardAI.Api.Services;
+
+public interface IJwtTokenService
+{
+    /// <summary>
+    /// Generates a signed JWT token for the given user.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <param name="email">The email address of the user.</param>
+    /// <returns>A signed JWT token string.</returns>
+    string GenerateToken(string userId, string email);
+}
