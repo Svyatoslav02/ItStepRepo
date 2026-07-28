53c4a34b8c2ea76b348a3afc667c7c873936d53d
svyatoslav <svyatoslavlot@gmail.com>
2026-07-28 23:25:37 +0300

    feature: add home page frontend changes

--- frontend/moodboard-frontend/src/routes/AppRoutes.jsx
+++ frontend/moodboard-frontend/src/routes/AppRoutes.jsx
@@ -1,14 +1,10 @@
 ﻿import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-import SignUpPage from '../pages/SignUpPage'
-import LoadingScreen from '../pages/LoadingScreen'
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/loading" element={<LoadingScreen />} />
         </Routes>
     )
 }
